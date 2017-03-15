dbb5c690eb2b19b40964fbcfef85d09069ca7827
Steve Iannaccone <renegadeoffunk1200@yahoo.com>
2017-03-15 12:35:12 -0600

    Fixing typos in webpage and changing port.

--- flask_predict.py
+++ flask_predict.py
@@ -1,10 +1,8 @@
 #############################################################################
 # Steve Iannaccone
 # March 2017
-#   Flask app for running NCAA March Madness predictions
 #
-# used this blog post as inspiration:
-#   https://medium.com/@amirziai/a-flask-api-for-serving-scikit-learn-models-c8bcdaa41daa#.q8gdx7oln
+# Flask app for running NCAA March Madness predictions
 #############################################################################
 from flask import Flask, request, url_for, render_template, request
 import cPickle as pickle
@@ -34,14 +32,17 @@ def pnt_predicter():
         spread = "Sorry! We can't find those teams..."
 
 
-    team1_img = 'img/TeamLogos/{0}.png'.format(process.extractOne(team1, all_team_imgs)[0])
+    team1_img = 'img/TeamLogos/{0}.png'.format(process.extractOne(team1,
+                                                all_team_imgs)[0])
     # team1_img_url = url_for('static', filename=team1_img)
 
-    team2_img = 'img/TeamLogos/{0}.png'.format(process.extractOne(team2, all_team_imgs)[0])
+    team2_img = 'img/TeamLogos/{0}.png'.format(process.extractOne(team2,
+                                                all_team_imgs)[0])
     # team2_img_url = url_for('static', filename=team2_img)
 
 
-    return render_template('predict.html', team1=team1, team2=team2, point_spread=spread, team1_img=team1_img, team2_img=team2_img)
+    return render_template('predict.html', team1=team1, team2=team2,
+            point_spread=spread, team1_img=team1_img, team2_img=team2_img)
 
 
 
@@ -49,4 +50,4 @@ if __name__ == '__main__':
     all_team_imgs = pickle.load(open('data/all_team_imgs', 'rb'))
     teamlist = pickle.load(open('data/allTeamList2016', 'rb'))
     predDict = pickle.load(open('data/predDict2017', 'rb'))
-    app.run(host='0.0.0.0', port=8080, threaded=True)
+    app.run(host='0.0.0.0', port=80, threaded=True)
