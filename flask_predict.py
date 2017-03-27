a05d782b45b45be1654d0e34720c9256e9c1a679
Steve Iannaccone <renegadeoffunk1200@yahoo.com>
2017-03-27 14:00:48 -0600

    Added upcoming games with lines from vegas and online sportsbooks compared to the STNS spread.

--- flask_predict.py
+++ flask_predict.py
@@ -34,15 +34,13 @@ def pnt_predicter():
 
     team1_img = 'img/TeamLogos/{0}.png'.format(process.extractOne(team1,
                                                 all_team_imgs)[0])
-    # team1_img_url = url_for('static', filename=team1_img)
 
     team2_img = 'img/TeamLogos/{0}.png'.format(process.extractOne(team2,
                                                 all_team_imgs)[0])
-    # team2_img_url = url_for('static', filename=team2_img)
 
 
     return render_template('predict.html', team1=team1, team2=team2,
-            point_spread=spread, team1_img=team1_img, team2_img=team2_img)
+            point_spread=spread, team1_img=team1_img, team2_img=team2_img, vegas_odds=vegas_odds)
 
 
 
@@ -50,4 +48,5 @@ if __name__ == '__main__':
     all_team_imgs = pickle.load(open('data/all_team_imgs', 'rb'))
     teamlist = pickle.load(open('data/allTeamList2016', 'rb'))
     predDict = pickle.load(open('data/predDict2017', 'rb'))
+    vegas_odds = pickle.load(open('data/vegas_odds', 'rb'))
     app.run(host='0.0.0.0', port=80, threaded=True)
